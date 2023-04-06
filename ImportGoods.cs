6d13c624d32a369da78de807a047eb84e0beba62
Son Nguyen <everestt2806@outlook.com.vn>
2023-04-06 14:53:20 +0700

    First time to create Project

---
+++ ImportGoods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Selling_Mobile_Phone_Products
+{
+    public partial class ImportGoods : Form
+    {
+        public ImportGoods()
+        {
+            InitializeComponent();
+        }
+
+        private void ImportGoods_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
